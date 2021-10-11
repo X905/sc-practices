711f287c3c199fee04198492385eeb22912de451
x905 <dbocastillo@gmail.com>
2021-10-11 05:18:49 -0600

    includes refactored methods and displays the status of threads

--- thread-multithread/Program.cs
+++ thread-multithread/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Globalization;
 using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+
 
 namespace thread_multithread
 {
     //this is an example of the use of threads 
     class Program
     {
+        static List<Thread> threads = new List<Thread>();
+
         static void Main(string[] args)
         {
             //example of normal operation on the default thread  
@@ -23,27 +29,41 @@ namespace thread_multithread
             {
                 Name = "Sentence Thread"
             };
+            threads.Add(sentenceThread);
+
             Thread numberThread = new Thread(RandomNumber)
             {
                 Name = "Number Thread"
             };
+
+            threads.Add(numberThread);
+
             Thread datetimeThread = new Thread(GetDateTime)
             {
                 Name = "Datetime Thread",
                 IsBackground = true,
             };
+
+            threads.Add(datetimeThread);
+
+            // Thread keyThread = new Thread(() => PrintKey("F"))
+            // {
+            //     Name = "Key Thread"
+            // };
             //Inician en este orden
             numberThread.Start();
             datetimeThread.Start();
             sentenceThread.Start();
+            // keyThread.Start();
             
-            PrintStatus(numberThread, datetimeThread, sentenceThread);
+            PrintStatus();
 
             Console.ReadKey();
         }
 
         //returns a randon sentence 
-        static void RandomSentence (){
+        static void RandomSentence ()
+        {
             Random rnd = new Random();
             string[] arr = new string[]{"Look","This","Football","Words","Styles","What","Programming","Rock","Repeat","The","He","Array"};
             int length = arr.Length;
@@ -53,46 +73,54 @@ namespace thread_multithread
             }
             Console.WriteLine("-------------------------------------------------------------------------");
             Console.WriteLine(output);
-            Console.WriteLine("RUNNING IN = " + Thread.CurrentThread.Name + " --- IS A BACKGROUND THREAD = " + Thread.CurrentThread.IsBackground);
+            Console.WriteLine($"RUNNING IN = {Thread.CurrentThread.Name}  --- IS A BACKGROUND THREAD = {Thread.CurrentThread.IsBackground}");
             Console.WriteLine("-------------------------------------------------------------------------");
 
         }
 
-        static void RandomNumber(){
+        static void RandomNumber()
+        {
             Thread.Sleep(10000);
             Random rnd = new Random();
 
             Console.WriteLine("-------------------------------------------------------------------------");
             Console.WriteLine(rnd.Next(1, int.MaxValue).ToString());
-            Console.WriteLine("RUNNING IN = " + Thread.CurrentThread.Name + " --- IS A BACKGROUND THREAD = " + Thread.CurrentThread.IsBackground);
+            Console.WriteLine($"RUNNING IN = {Thread.CurrentThread.Name}  --- IS A BACKGROUND THREAD = {Thread.CurrentThread.IsBackground}");
             Console.WriteLine("-------------------------------------------------------------------------");
         }
 
 
-        static void GetDateTime(){
+        static void GetDateTime()
+        {
             CultureInfo culture = new CultureInfo("en-US");
-
-            while(true){
+            int n = 10;
+            while(n > 1){
                 Thread.Sleep(10000);
                 Console.WriteLine("-------------------------------------------------------------------------");
                 Console.WriteLine(DateTime.Now.ToString("f",culture));
-                Console.WriteLine("RUNNING IN = " + Thread.CurrentThread.Name + " --- IS A BACKGROUND THREAD = " + Thread.CurrentThread.IsBackground);
+                Console.WriteLine($"RUNNING IN = {Thread.CurrentThread.Name}  --- IS A BACKGROUND THREAD = {Thread.CurrentThread.IsBackground}");
                 Console.WriteLine("-------------------------------------------------------------------------");
+                PrintStatus();
+                n--;
             }
         }
 
 
+        static void PrintKey(string key)
+        {
+             Console.WriteLine($"------------------------Pressed-Key--{key}---------------------------------");
+        }
 
-        static void PrintStatus(Thread numberThread, Thread datetimeThread, Thread sentenceThread){
+        static void PrintStatus()
+        {
+
+            Thread numberThread = threads.FirstOrDefault(x=>x.Name == "Number Thread") ,
+                    datetimeThread = threads.FirstOrDefault(x=>x.Name == "Datetime Thread"), 
+                    sentenceThread = threads.FirstOrDefault(x=>x.Name == "Sentence Thread");
             Console.WriteLine("_________________________________________________________________________");
-            Console.WriteLine("THREAD: " + sentenceThread.Name +
-                                " - IS ALIVE: " + sentenceThread.IsAlive);
-
-            Console.WriteLine("THREAD: " + datetimeThread.Name +
-                                " - IS ALIVE: " + datetimeThread.IsAlive);
-
-            Console.WriteLine("THREAD: " + numberThread.Name +
-                                " - IS ALIVE: " + numberThread.IsAlive);
+            Console.WriteLine($"THREAD: {sentenceThread.Name} - IS ALIVE: {sentenceThread.IsAlive}");
+            Console.WriteLine($"THREAD: {datetimeThread.Name} - IS ALIVE: { datetimeThread.IsAlive}");
+            Console.WriteLine($"THREAD: {numberThread.Name} - IS ALIVE:  {numberThread.IsAlive}");
             Console.WriteLine("_________________________________________________________________________");
         }
     }
