1b73bf158eb6c785a217a1317c99d4fc2d528f14
x905 <dbocastillo@gmail.com>
2021-10-11 00:22:06 -0600

    Multithread project started

---
+++ thread-multithread/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace thread_multithread
+{
+    //this is an example of the use of threads 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+        }
+
+        //returns a randon sentence 
+        static string RandomSentence (){
+            Random rnd = new Random();
+            string[] arr = new string[]{"Look","This","Football","Words","Styles","What","Programming","Rock","Repeat","The","He","Array"};
+            int length = arr.Length;
+            string output = "";
+            while(length > 1){
+                output += (" " + arr[rnd.Next(--length)]).ToLower();
+            }
+            return output;
+        }
+
+        static int RandomNumber(){
+            Random rnd = new Random();
+            return rnd.Next(1, int.MaxValue);
+        }
+
+
+        static string GetDateTime(){
+            CultureInfo culture = new CultureInfo("en-US");
+            return(DateTime.Now.ToString("f",culture));
+        }
+    }
+}
