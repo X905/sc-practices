8e5e681a1fd41253ec2070aea3b7c6129be95d4e
x905 <dbocastillo@gmail.com>
2021-10-11 01:22:12 -0600

    using main thread example

--- thread-multithread/Program.cs
+++ thread-multithread/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.Threading;
 
 namespace thread_multithread
 {
@@ -8,7 +9,19 @@ namespace thread_multithread
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            //example of normal operation on the default thread  
+            Thread currentThread = Thread.CurrentThread;
+            currentThread.Name = "Main thread";
+            Console.WriteLine(RandomSentence() + " --- RUNNING IN = " + currentThread.Name + " --- IS A BACKGROUND THREAD? " + currentThread.IsBackground);
+            Console.WriteLine(RandomNumber() + " --- RUNNING IN = " + currentThread.Name +  " --- IS A BACKGROUND THREAD? " + currentThread.IsBackground);
+            Console.WriteLine(GetDateTime() + " --- RUNNING IN = " + currentThread.Name +  " --- IS A BACKGROUND THREAD? " + currentThread.IsBackground);
+            Console.ReadLine();
+            Console.Clear();
+
+            //example of a multithread process
+
+
+            Console.Read();
         }
 
         //returns a randon sentence 
@@ -16,7 +29,7 @@ namespace thread_multithread
             Random rnd = new Random();
             string[] arr = new string[]{"Look","This","Football","Words","Styles","What","Programming","Rock","Repeat","The","He","Array"};
             int length = arr.Length;
-            string output = "";
+            string output = arr[0];
             while(length > 1){
                 output += (" " + arr[rnd.Next(--length)]).ToLower();
             }
