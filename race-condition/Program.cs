a9ba1c6a1cbb29fa42387ce0c3caa24d8d61edd1
x905 <dbocastillo@gmail.com>
2021-10-14 23:36:36 -0600

    race conditions projects

---
+++ race-condition/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace race_condition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxNames 100;
+            int maxThreadCount = 10;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
