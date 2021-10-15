a379c09c1a2513722e1681902cea0a8f84c7c695
x905 <dbocastillo@gmail.com>
2021-10-15 01:05:39 -0600

    Includes service bus project

---
+++ AzureServiceBus/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+
+namespace AzureServiceBus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
