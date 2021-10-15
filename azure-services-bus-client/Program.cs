23e23adcd37d945b527a42466886b018f9f4f408
x905 <dbocastillo@gmail.com>
2021-10-15 03:16:31 -0600

    Azure service bus client project

---
+++ azure-services-bus-client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace azure_services_bus_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
