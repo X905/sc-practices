6468837b2c02f7685ccfb0ebf37269d421cb5f0c
x905 <dbocastillo@gmail.com>
2021-10-15 02:46:51 -0600

    includes message sender

--- AzureServiceBus/Program.cs
+++ AzureServiceBus/Program.cs
@@ -6,9 +6,56 @@ namespace AzureServiceBus
 {
     class Program
     {
-        static void Main(string[] args)
+        // connection string to your Service Bus namespace
+        static string connectionString = "CONNECTION STRING HERE";
+
+        // name of your Service Bus queue
+        static string queueName = "myqueue";
+
+        // the client that owns the connection and can be used to create senders and receivers
+        static ServiceBusClient client;
+
+        // the sender used to publish messages to the queue
+        static ServiceBusSender sender;
+
+        // number of messages to be sent to the queue
+        private const int numOfMessages = 3;
+
+        static async Task Main()
         {
-            Console.WriteLine("Hello World!");
+        
+            client = new ServiceBusClient(connectionString);
+            sender = client.CreateSender(queueName);
+
+            //Create a batch
+            using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
+
+            for (int i = 1; i <= numOfMessages; i++)
+            {
+                // try adding a message to the batch
+                if (!messageBatch.TryAddMessage(new ServiceBusMessage($"Message {i}")))
+                {
+                    // if it is too large for the batch
+                    throw new Exception($"The message {i} is too large to fit in the batch.");
+                }
+            }
+
+            try
+            {
+                // Use the producer client to send the batch of messages to the Service Bus queue
+                await sender.SendMessagesAsync(messageBatch);
+                Console.WriteLine($"A batch of {numOfMessages} messages has been published to the queue.");
+            }
+            finally
+            {
+                // Calling DisposeAsync on client types is required to ensure that network
+                // resources and other unmanaged objects are properly cleaned up.
+                await sender.DisposeAsync();
+                await client.DisposeAsync();
+            }
+
+            Console.WriteLine("Press any key to end the application");
+            Console.ReadKey();
         }
     }
 }
